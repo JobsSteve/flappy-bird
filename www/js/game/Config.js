820883e77d8487b74a5e9f16ad418815b9a61f4d
Eugene Obrezkov <ghaiklor@gmail.com>
2014-02-14 10:02:59 +0200

    Added jquery mobile. Realzed basic navitgation.

--- www/js/game/Config.js
+++ www/js/game/Config.js
@@ -1,18 +1,6 @@
 var Game = (function(window, document, Game) {
 
-    Game.Config = {
-        Activities: {
-            menuId: 'game-activity-menu',
-            sceneId: 'game-activity-scene',
-            recordsId: 'game-activity-records'
-        },
-        Buttons: {
-            'backToMainMenuButton': Game.showMainMenu,
-            'startNewGameButton': Game.startNewGame,
-            'showHighscoresButton': Game.showHighscores,
-            'exitFromGameButton': Game.exit
-        }
-    };
+    Game.Config = {};
 
     return Game;
 })(window, document, window.Game || {});
